9f6dd8609d605b3cfbd2db57f28adf36a89b8e0b
Katerina Chrysostomou <katerina.chrysostomou@hotmail.com>
2022-01-19 16:02:19 +0100

    added some css and a external fruit source.

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 import { shuffle } from "lodash-es";
 // här importerar vi shuffle från lodash-es, så slipper vi skriva tusen rader med kod för att shuffla!
+import getRandomFruitsName from "random-fruits-name";
 
-const ingredients = [
-  "Banana",
-  "Mango",
-  "Orange juice",
-  "Sorbet",
-  "Silken tofu",
-  "Nut butters",
-  "Avocado",
-  "Bananas",
-  "Pineapple",
-  "Strawberries",
-  "Blueberries",
-  "Raspberries",
-  "Blackberries",
-  "Cherries",
-  "Peaches",
-  "Cantaloupe",
-  "Watermelon",
-  "Pomegranate seeds",
-  "Kiwi",
-  "Acai berries",
-];
+function getFiveRandomFruits() {
+  const fruits = [];
+
+  for (let i = 0; i < 5; i++) {
+    const name = getRandomFruitsName();
+    fruits.push(name);
+  }
+  return fruits;
+}
+
+const name = getRandomFruitsName();
+
+// const ingredients = [
+//   "Banana",
+//   "Mango",
+//   "Orange juice",
+//   "Sorbet",
+//   "Silken tofu",
+//   "Nut butters",
+//   "Avocado",
+//   "Bananas",
+//   "Pineapple",
+//   "Strawberries",
+//   "Blueberries",
+//   "Raspberries",
+//   "Blackberries",
+//   "Cherries",
+//   "Peaches",
+//   "Cantaloupe",
+//   "Watermelon",
+//   "Pomegranate seeds",
+//   "Kiwi",
+//   "Acai berries",
+// ];
 
 const button = document.querySelector("button");
 const ul = document.querySelector("ul");
 
 function shuffleAndRender() {
   // Här shufflar vi ingredienserna!
-  const shuffledIngredients = shuffle(ingredients);
+  const ingredients = getFiveRandomFruits();
 
-  // Här tar vi fram 5 ingredienser ur vår lista!
-  for (let i = 0; i < 5; i++) {
-    const ingredient = shuffledIngredients[i];
-
+  ingredients.forEach((ingredient) => {
     const li = document.createElement("li");
     li.textContent = ingredient;
     ul.append(li);
-  }
+  });
 }
 
 button.addEventListener("click", () => {
   //   Här är så att vi tar bort texten som finns sen innan, när vi trycker på knappen igen.
   ul.textContent = null;
   shuffleAndRender();
-  console.log("yay!");
 });
